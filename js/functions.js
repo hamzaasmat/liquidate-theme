477baa3a169c5e9ea1b85513f9c021551f80621b
Hamza Asmat <hamza.asmat@ideatolife.me>
2023-01-04 00:03:28 +0500

    Adding files

---
+++ js/functions.js
@@ -0,0 +1,6 @@
+// var menuBtn = document.getElementsByClassName('.app-header__menu-button');
+
+// menuBtn.addEventListener('click', openSidebar);
+function openSidebar() {
+    alert('working')
+}
